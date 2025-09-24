16f70dce5f2cc12fb27cfaf35ca1528765c38c27
abdulla-17 <abd26437@gmail.com>
2025-09-24 14:10:57 +0530

    initialize books SQL script

--- online-bookstore/books.sql
+++ online-bookstore/books.sql
@@ -27,9 +27,8 @@ WHERE title = 'Deep Work';
 DELETE FROM books
 WHERE title = 'Ikigai';
 
-SELECT AVG(price) AS average_price,
-       COUNT(*) AS total_books
-FROM books;
+SELECT AVG(price),COUNT(*)
+FROM books1
 
 SELECT *
 FROM books
