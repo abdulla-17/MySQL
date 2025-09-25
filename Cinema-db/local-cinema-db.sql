e7de5cd410109bfaaec164e13dea7b8a90c94445
abdulla-17 <abd26437@gmail.com>
2025-09-25 14:15:13 +0530

    initialize cinema database SQL script

---
+++ Cinema-db/local-cinema-db.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE cinema;
+
+CREATE TABLE movies(
+    mid int(10) PRIMARY KEY,
+    title varchar(50),
+    genre varchar(20),
+    realease_year int(4),
+    rating float(2,1),
+    box_office int(10));
+
+INSERT INTO movies
+VALUES
+(1, 'Inception', 'Sci-Fi', 2010, 8.8, 825),
+(2, 'The Dark Knight', 'Action', 2008, 9.0, 1004),
+(3, 'Interstellar', 'Sci-Fi', 2014, 8.6, 677),
+(4, 'Joker', 'Drama', 2019, 8.4, 1074),
+(5, 'Dunkirk', 'War', 2017, 7.9, 527);
+
+SELECT title, box_office AS "Earnings (in Cr)"
+FROM movies;
+
+SELECT title AS "Movie Title", genre AS "Category"
+FROM movies;
+
+SELECT title, genre, rating AS "IMDb Score"
+FROM movies
+WHERE genre IN ('Sci-Fi', 'Action');
+
+SELECT title, release_year AS "Release"
+FROM movies
+WHERE release_year BETWEEN 2008 AND 2015;
+
+SELECT COUNT(*) AS "High Rated Movies"
+FROM movies
+WHERE rating > 8.5;
